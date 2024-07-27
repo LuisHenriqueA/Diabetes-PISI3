556b0c2c6c5e556b6df887389c688748965d44d5
Luis Abreu <luishdasilvaa335@gmail.com>
2024-07-27 17:12:45 -0300

    Estilização

---
+++ Home.py
@@ -0,0 +1,64 @@
+import streamlit as st
+from utils.header import add_header
+
+def build_page():
+    st.markdown('''
+        <h1>Aprendizado de máquina aplicado no contexto de Diabetes</h1>
+        <br>
+        O presente estudo aborda a Diabetes Mellitus (DM) como um distúrbio metabólico que afeta milhões de pessoas globalmente, que destaca-se pela sua complexidade e impacto na qualidade de vida dos pacientes.
+        Com o aumento da prevalência da doença, surge a importância de estudos para entender o impacto da Diabetes na vida dos pacientes. 
+        Nesse contexto, a aplicação de técnicas de aprendizado de máquina, algoritmos de clusterização e classificação, 
+        surge como uma ferramenta  para analisar dados e identificar padrões que indicam impactos causados nos pacientes diabéticos e pré-diabéticos por conta da doença,
+        essa pesquisa visa utilizar as técnicas de aprendizado de máquina na base de dados Diabetes Health Indicator Dataset para conduzir sua análise e investigação. 
+        <br>
+    ''', unsafe_allow_html=True)
+
+def render_page(page_name):
+    if page_name == "Home🏠":
+        build_page()
+    elif page_name == "Dashboards📈":
+        import Screens.Dashboards
+        Screens.Dashboards.build_page()
+        
+# Opções de navegação
+def configure_sidebar():
+    st.sidebar.markdown(
+        """
+        <style>
+        .stRadio *{
+            color: #FBFAF3;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    st.sidebar.markdown('''
+        <style>
+        .sidebar_h2{
+            color: #FBFAF3;
+            }              
+        </style>
+        <h2 class="sidebar_h2">Escolha uma página</h2>
+                        
+    ''', unsafe_allow_html=True)
+    
+    page = st.sidebar.radio("Páginas", ["Home🏠", "Dashboards📈"], key='sidebar',  label_visibility="hidden")
+    return page
+
+def main():
+    st.set_page_config(
+    page_title = "PISI 3 - Diabetes",
+    layout = "wide",
+    menu_items = {
+        'About': '''Sistema desenvolvido para cadeira de PISI 3
+        Tema: Diabetes
+        Autores: Alana Lins, Davi Vieira, David Erick, Luis Henrique
+        Dataset link: www.kaggle.com/datasets/alexteboul/diabetes-health-indicators-dataset
+        '''
+    }
+)
+    add_header()
+    page = configure_sidebar()
+    render_page(page)
+
+main()
--- Screens/Dashboards.py
+++ Screens/Dashboards.py
@@ -13,8 +13,8 @@ def build_page():
 
 def build_header():
     text='<h1>Gráficos com base de diabetes</h1>'+\
-    '<p>Esta página apresenta a partir da seguinte base de dados importada do'+\
-    'Kaggle¹ (https://www.kaggle.com/datasets/alexteboul/diabetes-health-indicators-dataset).</p>'
+    '<p>Esta página apresenta informações a partir da seguinte base de dados importada do '+\
+    '<a href="https://www.kaggle.com/datasets/alexteboul/diabetes-health-indicators-dataset">Kaggle</a>¹ .</p>'
     st.write(text, unsafe_allow_html=True)
 
 def build_body():
@@ -29,18 +29,15 @@ def build_diabetesplot_section(df:pd.DataFrame):
     c1, c2 = st.columns([.3,.7])
     cols = ['Fumante','Cobertura_de_saúde','Atividades_físicas']
     selec_col = c1.selectbox('Seleção', options=cols, key='selec_1')
-    stacked = c1.checkbox('Empilhado', value=True)
-    if stacked:
-        fig = create_histograma_stacked(df, selec_col)
-    else:
-        fig = create_histograma_stacked(df, selec_col)
+    #stacked = c1.checkbox('Empilhado', value=True)
+    #if stacked:
+    fig = create_histograma_stacked(df, selec_col)
+    #else:
+    #    fig = create_histograma_stacked(df, selec_col)
     fig.update_layout(title=f'Histograma de {selec_col} por condição diabética.',
                       legend_title_text=selec_col, xaxis_title_text='Condição', yaxis_title_text='Quantidade')
     c2.plotly_chart(fig, use_container_width=True)
     
 def create_histograma_stacked(df:pd.DataFrame, series_col:str) -> go.Figure:
     df = df.query(f'{series_col}.notna()').copy()
-    #df.sort_values(by=series_col, inplace=True)
     return px.histogram(df, x='Diabetes', nbins=20, color=series_col, opacity=.75)
-
-build_page()
